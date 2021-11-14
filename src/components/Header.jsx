b1ceb6cbb4ff8531b1095b3ba059362baa293b57
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-13 23:20:56 -0500

    using gravatar to confirm login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,28 +2,45 @@ import React from "react";
 import "../assets/styles/components/Header.scss";
 import logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import gravatar from "../utils/gravatar";
 
-const Header = () => (
-  <header className="header">
-    <Link to="/">
-      <img className="header__img" src={logo} alt="Platzi Video" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
+const Header = (props) => {
+  const { user } = props;
+  const isLogin = Object.keys(user).length > 0;
+  return (
+    <header className="header">
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {isLogin ? (
+            <img src={gravatar(user.email)} alt="userPhoto" />
+          ) : (
+            <img src={userIcon} alt="" />
+          )}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <Link to="/register">Cuenta</Link>
+          </li>
+          <li>
+            <Link to="/login">Iniciar Sesión</Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Link to="/register">Cuenta</Link>
-        </li>
-        <li>
-          <Link to="/login">Iniciar Sesión</Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    ...state,
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, null)(Header);
--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -2,7 +2,10 @@ import md5 from "md5";
 
 const gravatar = (email) => {
   const base = "https://gravatar.com/avatar/";
-  const formatEmail = email.toLowerCase().trim();
-  const hash = md5(formatEmail, { encoding: "binary" });
-  return `${base}${hash}`;
+  const formattEmail = email.trim().toLowerCase();
+  const hash = md5(formattEmail, { encoding: "binary" });
+
+  return `${base}${hash}?d=identicon`;
 };
+
+export default gravatar;
